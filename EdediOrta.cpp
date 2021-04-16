3f6c5f462514a6d3427041f8ac491488e4853936
sirincay <80684361+sirincay@users.noreply.github.com>
2021-04-16 19:30:31 +0400

    Create EdediOrta.cpp

---
+++ EdediOrta.cpp
@@ -0,0 +1,16 @@
+#include <iostream>  // Ededi Orta C++ Telegram-@SirinCayBoos 
+#include <conio.h>
+using namespace std;
+ int main() {
+ 	int x1,x2,x3;
+ 	double orta;
+ 	cout<<"1ci Reqemi Daxil Et : ";
+ 	cin>>x1;
+ 	cout<<"2ci Reqemi Daxil Et : ";
+ 	cin>>x2;
+ 	cout<<"3cu Reqemi Daxil Et : ";
+ 	cin>>x3;
+ 	orta=(double)(x1+x2+x3)/3;
+ 	cout<<"Ededi Orda : "<<orta;
+ 	getch();
+ }
